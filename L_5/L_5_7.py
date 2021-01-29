d5b0704216f9f9469413807c9299058cb8907e16
Roman Loginov <loginov.roman@gmail.com>
2021-01-29 17:23:05 +0300

    Lesson 5

--- L_5/L_5_7.py
+++ L_5/L_5_7.py
@@ -1,13 +1,19 @@
+import json
 file = open("l57.txt", "r")
 
 average = 0
 i = 0
+pnls = {}
 for line in file:
     firm, tp, pstr, lstr = line.split()
-    income = int(pstr) - int(lstr)
-    if income > 0:
+    pnl = int(pstr) - int(lstr)
+    pnls[firm] = pnl
+    if pnl > 0:
         i += 1
-        average += income
+        average += pnl
 print(f"Average income is {(average / i) if i > 0 else 0}")
+avg_obj = {"average_profit": average}
 file.close()
+with open("l57.json", "w") as outf:
+    json.dump([pnls, avg_obj], outf, ensure_ascii = False, indent = 4)
 
